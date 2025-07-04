a9e0a5285e9b3366194e0cbcbdea57ba52f3bf5a
guava <yuvayashvin@gmail.com>
2025-07-04 12:12:43 +0530

    added 404 html page and routes

--- app.py
+++ app.py
@@ -55,5 +55,9 @@ def result():
 
     return render_template('predict.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
